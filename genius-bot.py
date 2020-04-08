2d96ab80a249befea4d10904341dd6b1e1adef6f
Knucklessg1 <knucklessg1@gmail.com>
2020-04-08 01:26:06 -0500

    Final tweaks on base

--- genius-bot.py
+++ genius-bot.py
@@ -13,6 +13,7 @@ from matplotlib.figure import Figure
 import numpy as np
 import queue
 import re
+import os
 
 
 class GeniusBot:
@@ -37,6 +38,7 @@ class GeniusBot:
         tk.Grid.rowconfigure(self.main_frame, 1, minsize=1, weight=0)
         tk.Grid.rowconfigure(self.main_frame, 2, minsize=1, weight=1)
         tk.Grid.rowconfigure(self.main_frame, 3, minsize=1, weight=0)
+        tk.Grid.rowconfigure(self.main_frame, 4, minsize=1, weight=0)
         tk.Grid.columnconfigure(self.main_frame, 0, minsize=1, weight=1)
         self.top_frame = tk.Frame(self.main_frame)
         tk.Grid.rowconfigure(self.top_frame, 0, minsize=1, weight=0)
@@ -56,7 +58,9 @@ class GeniusBot:
         tk.Grid.rowconfigure(self.bottom_frame, 0, minsize=1, weight=1)
         tk.Grid.columnconfigure(self.bottom_frame, 0, minsize=1, weight=1)
         tk.Grid.columnconfigure(self.bottom_frame, 1, minsize=1, weight=1)
-        self.notification_frame = tk.Frame(self.root)
+        self.notification_frame = tk.Frame(self.main_frame)
+        tk.Grid.rowconfigure(self.notification_frame, 0, minsize=1, weight=1)
+        tk.Grid.columnconfigure(self.notification_frame, 0, minsize=1, weight=1)
         self.main_frame.grid(row=0, column=0, sticky='NSEW')
         self.top_frame.grid(row=0, column=0, sticky='NSEW')
         self.middle_button_frame.grid(row=1, column=0, sticky='NSEW')
@@ -78,9 +82,14 @@ class GeniusBot:
 
         # Labels
         self.title = tk.StringVar()
-        self.title.set("GeniusBot - Web Archive")
+        self.title.set("Genius - Web Archive")
         self.title_label = tk.Label(self.top_frame, textvariable=self.title)
         self.title_label.grid(column=0, row=0, sticky='NSEW', columnspan=1)
+        self.status = tk.StringVar()
+        self.status.set("Welcome - Please begin by entering YouTube URLs")
+        self.status_label = tk.Label(self.notification_frame, bd=1, textvariable=self.status, anchor='w', relief=tk.SUNKEN)
+        self.status_label.grid(column=0, row=0, sticky='NSEW', columnspan=1)
+        #self.status_label.pack(side=tk.BOTTOM, fill=tk.X)
         self.queue_title = tk.StringVar()
         self.queue_title.set("Video Download Queue")
         self.queue_title_label = tk.Label(self.middle_frame, textvariable=self.queue_title)
@@ -88,7 +97,7 @@ class GeniusBot:
         self.w_text = tk.StringVar()
         self.w_text.set("Enter YouTube Link(s) Below: ")
         self.w = tk.Label(self.top_frame, textvariable=self.w_text)
-        self.w.grid(column=0, row=1, columnspan=2, sticky='NSEW')
+        self.w.grid(column=0, row=1, columnspan=2, sticky='W')
         self.percentage_text = tk.StringVar()
         self.percentage_text.set(f"{self.value}/{self.max_value} | {(self.value/(self.max_value+1))*100}%")
         self.percentage_label = ttk.Label(self.bottom_frame, textvariable=self.percentage_text)
@@ -106,6 +115,7 @@ class GeniusBot:
         # Entries
         self.url_entry = tk.Text(self.top_frame, height=9)
         tk.Grid.columnconfigure(self.url_entry, 0, weight=1)
+        self.url_entry.bind("<Tab>", self.focus_next_widget)
         self.refresh_list()
         self.url_entry.grid(column=0, row=2, columnspan=3, sticky='NSEW')
 
@@ -119,8 +129,13 @@ class GeniusBot:
     def run(self, func, name=None):
         threading.Thread(target=func, name=name).start()
 
+    # This class handles [TAB] Key to move to next Widget
+    def focus_next_widget(self, event):
+        event.widget.tk_focusNext().focus()
+        return("break")
+
     def open_file(self):
-        name = askopenfilename(initialdir="C:/Users/Batman/Documents/Programming/tkinter/",
+        name = askopenfilename(initialdir=os.getcwd(),
                                filetypes=(("Text File", "*.txt"), ("All Files", "*.*")),
                                title="Choose a file."
                                )
@@ -132,11 +147,10 @@ class GeniusBot:
             for url in youtube_urls:
                 self.url_list.append(url)
             self.refresh_list()
-
-            print("Length of Links After Open File: ", len(self.url_list))
-            print(name)
+            self.status.set(f'Loaded {len(self.url_list)} URLs from file: {name}')
         except:
             print("No file exists")
+            self.status.set(f'File Not Found')
 
     def refresh_list(self):
         self.url_listbox.delete(0, tk.END)
@@ -148,17 +162,31 @@ class GeniusBot:
         parse_addition = self.url_entry.get("1.0", tk.END)
         parse_addition_array = parse_addition.splitlines()
         for url in parse_addition_array:
-            temp = re.sub(r'[^A-Za-z0-9_./:?!=]', '', url)
-            self.url_list.append(temp)
+            if re.sub(r'[^A-Za-z0-9_./:?!=]', '', url) != "":
+                self.status.set(f'Added URLs to Queue')
+                temp = re.sub(r'[^A-Za-z0-9_./:?!=]', '', url)
+                self.url_list.append(temp)
+            else:
+                print("Bad URL: ", url)
+                self.status.set(f'Empty URL: {url}')
+        self.url_list = list(dict.fromkeys(self.url_list))
         self.url_entry.delete("1.0", tk.END)
         self.refresh_list()
 
     def remove_url(self):
-        print("Removing URL: ", self.url_listbox.get(self.url_listbox.curselection()))
-        self.url_list.remove(self.url_listbox.get(self.url_listbox.curselection()))
-        self.refresh_list()
+        if self.url_listbox.curselection():
+            url = self.url_listbox.get(self.url_listbox.curselection())
+            url = url.rstrip()
+            self.status.set(f'Removed URL: {url}')
+            print("Removing URL: ", self.url_listbox.get(self.url_listbox.curselection()))
+            self.url_list.remove(self.url_listbox.get(self.url_listbox.curselection()))
+            self.refresh_list()
+        else:
+            print("Click on a link to remove")
+            self.status.set(f'Click on a URL to remove')
 
     def download_video_threaded(self, tkt_wrap):
+        self.status.set(f'Downloading {len(self.url_list)} URLs')
         self.download_button_video["state"] = "disabled"
         self.download_button_audio["state"] = "disabled"
         self.value = 0
@@ -184,11 +212,14 @@ class GeniusBot:
             print("Max Value: ", self.max_value)
             # tkt_wrap(entry.delete, '0', 'end')
             # tkt_wrap(entry.set(txt))
+            self.status.set(f'Completed {self.value}/{self.max_value}')
             i += 1
         self.download_button_video["state"] = "enabled"
         self.download_button_audio["state"] = "enabled"
+        self.status.set(f'Downloaded {self.value} videos!')
 
     def download_audio_threaded(self, tkt_wrap):
+        self.status.set(f'Downloading {len(self.url_list)} URLs')
         self.download_button_video["state"] = "disabled"
         self.download_button_audio["state"] = "disabled"
         self.value = 0
@@ -213,9 +244,11 @@ class GeniusBot:
             print("Max Value: ", self.max_value)
             # tkt_wrap(entry.delete, '0', 'end')
             # tkt_wrap(entry.set(txt))
+            self.status.set(f'Completed {self.value}/{self.max_value}')
             i += 1
         self.download_button_video["state"] = "enabled"
         self.download_button_audio["state"] = "enabled"
+        self.status.set(f'Downloaded {self.value} videos!')
 
     def download_videos(self):
         self.run(lambda: self.download_video_threaded(self.tkt.nosync), name='NoSync')
@@ -226,7 +259,7 @@ class GeniusBot:
 
 root = tk.Tk()
 root.title("Genius Web Archiver")
-#root.geometry("500x725")
+root.geometry("500x700")
 root.minsize(500, 700)
 tkt = TkThread(root)  # make the thread-safe callable
 main_ui = GeniusBot(root, tkt)
