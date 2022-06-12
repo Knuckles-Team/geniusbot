00a75d89808f673d512e1d5784abf5d89bc015ab
Knucklessg1 <knucklessg1@gmail.com>
2022-06-12 11:11:55 -0500

    Build new version with icons included.

--- build_container.sh
+++ build_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t cryptobot:latest -f "./Dockerfile" .
-docker run -v /mnt:/mnt -it cryptobot:latest bash
+docker build -t geniusbot:latest -f "./Dockerfile" .
+docker run -v /mnt:/mnt -it geniusbot:latest bash
