2e90540208a247f6161d1f90c1c8852d4f9d765b
Knucklessg1 <knucklessg1@gmail.com>
2022-06-18 15:50:38 -0500

    Testing chatterbot fixes.

--- geniusbot/chatty.py
+++ geniusbot/chatty.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+chatbot = ChatBot("Don Ovan")
+
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome."
+]
+
+trainer = ListTrainer(chatbot)
+
+trainer.train(conversation)
+
+response = chatbot.get_response("Good morning!")
+print(response)
