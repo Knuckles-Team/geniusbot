b6f2e2a0c65f64b6282cd579b794ca154a211183
Knucklessg1 <knucklessg1@gmail.com>
2022-07-31 11:21:40 -0500

    Loading with cpu.

--- geniusbot/geniusbot_chat.py
+++ geniusbot/geniusbot_chat.py
@@ -11,15 +11,22 @@ def save_model():
     model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
     # save model with torch.save
     print("Saving model")
-    torch.save(model, "gptj.pt")
+    torch.save(model, "geniusbot_gptj.pt")
     print("Model Saved")
 
 
 def load_model():
     print("Loading Model")
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    #torch.device('cpu')
     # load model
-    model = torch.load("gptj.pt")#.to("cuda",  torch_dtype=torch.float16)
-    #model = torch.load("gptj.pt", map_location=torch.device('cpu'))
+    #model = torch.load("geniusbot_gptj.pt")#.to("cuda",  torch_dtype=torch.float16)
+    #model = torch.load("geniusbot_gptj.pt", map_location=torch.device('cpu'))
+    model = GPTJForCausalLM.from_pretrained("EleutherAI/gpt-j-6B", revision="float16", torch_dtype=torch.float16, low_cpu_mem_usage=True)
+    model.load_state_dict(torch.load("geniusbot_gptj.pt"))
+    model.device(device)
+    # model = Model()
+    # model.load_state_dict(torch.load("geniusbot_gptj.pt", map_location=torch.device('cpu')))
     # torch.load('tensors.pt', map_location=lambda storage, loc: storage.cuda(1))
     # load tokenizer
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
@@ -33,5 +40,5 @@ def load_model():
     print("Model Loaded")
 
 
-save_model()
+#save_model()
 load_model()
