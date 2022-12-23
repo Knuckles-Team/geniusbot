d58b342147fe3284d0ced7cc98d3b558d69e928e
AUdel <knucklessg1@gmail.com>
2022-12-23 13:00:10 -0600

    Use requirements.txt for instal_reqs in setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@
 from setuptools import setup
 from geniusbot.version import __version__, __author__
 from pathlib import Path
+import os
 import re
+from pip._internal.network.session import PipSession
+from pip._internal.req import parse_requirements
 
 readme = Path('README.md').read_text()
 version = __version__
+requirements = parse_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'), session=PipSession())
 readme = re.sub(r"Version: [0-9]*\.[0-9]*\.[0-9]+", f"Version: {version}", readme)
-print(f"README: {readme}")
 with open("README.md", "w") as readme_file:
     readme_file.write(readme)
 description = 'The Ever-learning and ever-improving tool!'
@@ -28,12 +31,7 @@ setup(
         'geniusbot',
     ],
     include_package_data=True,
-    install_requires=[
-        'PyQt5>=5.15.7', 'winshell>=0.6; platform_system == "Windows"', 'pypiwin32; platform_system == "Windows"',
-        'torch>=1.13.1', 'transformers>=4.25.1', 'accelerate>=0.15.0', "tabulate>=0.9.0", "psutil>=5.9.4",
-        'media-downloader>=0.1.1', 'webarchiver>=0.4.0', 'subshift>=0.6.0', 'genius-chatbot>=0.3.0',
-        'media-manager>=0.1.1', 'report-manager>=0.6.0', 'repository-manager>=0.2.1', 'systems-manager>=0.2.0',
-    ],
+    install_requires=[str(requirement.requirement) for requirement in requirements],
     py_modules=['geniusbot'],
     package_data={'geniusbot': ['geniusbot']},
     data_files=[
@@ -44,8 +42,6 @@ setup(
         'License :: Public Domain',
         'Environment :: Console',
         'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
