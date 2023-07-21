c9ad022c211b3fba8edf7e3f723f0fabce74f509
knucklessg1 <knucklessg1@gmail.com>
2023-07-21 00:07:07 -0500

    Updated support for newest geniusbot_chat_plugin.py

--- geniusbot/plugins/geniusbot_chat_plugin.py
+++ geniusbot/plugins/geniusbot_chat_plugin.py
@@ -86,24 +86,18 @@ class GeniusBotWorker(QObject):
         self.geniusbot_chatbot = geniusbot_chatbot
         self.geniusbot_chat = geniusbot_chat
         self.text = text
-        self.default_text = "Hello, my name is Geniusbot and I'm an artificially intelligent robot that can help you with"
+        self.default_text = "Hello, my name is Geniusbot and I'm an artificially intelligent robot that can help you with anything you need!"
 
     def run(self):
         """Long-running task."""
         old_text = self.geniusbot_chat.text()
-        if self.geniusbot_chatbot.get_loaded() is False:
-            self.geniusbot_chat.setText(f"{self.geniusbot_chat.text()}\n"
-                                        f"[Genius Bot] Attempting to load intelligence...")
-            self.geniusbot_chatbot.set_output_length(output_length=500)
-            self.geniusbot_chatbot.scale_intelligence()
-            self.geniusbot_chatbot.load_model()
-            self.geniusbot_chat.setText(f"{self.geniusbot_chat.text()}\n"
-                                        f"[Genius Bot] Loaded {self.geniusbot_chatbot.get_intelligence_level()} "
-                                        f"intelligence level!")
+        self.geniusbot_chat.setText(f"{self.geniusbot_chat.text()}\n"
+                                    f"[Genius Bot] Firing up the gears...")
+        self.geniusbot_chatbot.source_directory = os.path.dirname((os.path.normpath(os.path.dirname(__file__))))
+        self.geniusbot_chatbot.assimilate()
         if self.text == '':
             self.text = self.default_text
-        self.geniusbot_chat.setText(f"{old_text}\n[Genius Bot] Processing ...")
         response = self.geniusbot_chatbot.chat(prompt=self.text)
-        self.geniusbot_chat.setText(f"{old_text}\n[Genius Bot] {response}")
+        self.geniusbot_chat.setText(f"{old_text}\n[Genius Bot] {response['answer']}\n[Source] {response['source']}")
         self.progress.emit(100)
         self.finished.emit()
